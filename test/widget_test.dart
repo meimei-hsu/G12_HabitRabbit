fa174b91654e892f336e370cf4145be691ea7a06
Meimei <maggie10135@gmail.com>
2023-04-04 09:28:40 +0800

    Debugging widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:G12_4myfat/main.dart';
+import 'package:g12/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
