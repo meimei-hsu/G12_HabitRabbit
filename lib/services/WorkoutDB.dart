b25418b33ed7a1f0ff1657834a9b009b6e40ee84
jenniferjj033 <jenniferjj033@gmail.com>
2023-04-17 19:24:58 +0800

    workout DB

--- lib/services/WorkoutDB.dart
+++ lib/services/WorkoutDB.dart
@@ -29,12 +29,77 @@ class WorkoutDB {
     return [
       "workoutId",
       "workoutName",
-      "workoutName",
+      "workoutType",
       "workoutDifficulty",
 
     ];
   }
 
+  // Select
+  static Future<List<Workout>> getUserList() async {
+    final Database db = await initDatabase();
+    final List<Map<String, dynamic>> maps = await db.query("USER");
+
+    return List.generate(maps.length, (i) {
+      return Workout(
+        workoutId: maps[i]["workoutId"],
+        workoutName: maps[i]["workoutName"],
+        workoutType: maps[i]["workoutType"],
+        workoutDifficulty: maps[i]["workoutDifficulty"],
+
+      );
+    });
+  }
+
+  /*static Future<Workout?> getUser(String email) async {
+    final List<Workout> users = await getUserList();
+    for (Workout workout in users) {
+      if (workout.email == email) {
+        return workout;
+      }
+    }
+    return null;
+  }
+
+  static Future<List<Map>?> getDynamicData(String email) async {
+    final User? user = await getUser(email);
+
+    if (user != null) {
+      return [
+        {
+          'height': user.height,
+          'weight': user.weight,
+          'timeSpan': user.timeSpan,
+        },
+        Map.fromIterables(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            user.workoutDays.split('')),
+        {
+          'strengthLiking': user.strengthLiking,
+          'cardioLiking': user.cardioLiking,
+          'yogaLiking': user.yogaLiking,
+        },
+        {
+          'strengthAbility': user.strengthAbility,
+          'cardioAbility': user.cardioAbility,
+          'yogaAbility': user.yogaAbility,
+        },
+      ];
+    } else {
+      return null;
+    }
+  }
+
+  static Future<String> getPassword(String email) async {
+    final Database db = await initDatabase();
+    final List<Map<String, dynamic>> maps =
+    await db.rawQuery("SELECT password FROM USER WHERE email = '$email'");
+    if (maps.isEmpty) {
+      return '';
+    } else {
+      return maps[0]["password"];
+    }
+  }
+*/
   // Insert
   static Future<bool> insertWorkout(Workout workout) async {
     final Database db = await initDatabase();
@@ -47,15 +112,114 @@ class WorkoutDB {
   }
 
   // Update
-  static Future<bool> updateUser(Workout workout) async {
+  /*static Future<bool> updateWorkout(Workout workout) async {
     final db = await initDatabase();
     int count = await db.update(
       "WORKOUT",
-      Workout.toMap(),
+      workout.toMap(),
       where: "email = ?",
       whereArgs: [user.email],
     );
     return (count != 0) ? true : false;
   }
+
+  static Future<bool> updateData(
+      String email, Map<String, dynamic> data) async {
+    final db = await initDatabase();
+    int count = await db.update(
+      "USER",
+      data,
+      where: "email = ?",
+      whereArgs: [email],
+    );
+    return (count != 0) ? true : false;
+  }
+
+  // Delete
+  static Future<bool> deleteUser(String email) async {
+    final db = await initDatabase();
+    int count = await db.delete(
+      "USER",
+      where: "email = ?",
+      whereArgs: [email],
+    );
+    return (count != 0) ? true : false;
+  }*/
 }
 
+class Workout {
+  // Init
+   String workoutId, workoutName,workoutType;
+  //final String email, gender, birthday;
+   int workoutDifficulty;
+
+
+  Workout({
+    required this.workoutId,
+    required this.workoutName,
+    required this.workoutType,
+    required this.workoutDifficulty,
+  });
+
+  // Setter
+  void setData(Map<String, dynamic> data) {
+    workoutId = data['workoutId'] ?? workoutId;
+    workoutName = data['workoutName'] ?? workoutName;
+    workoutType = data['workoutType'] ?? workoutType;
+    workoutDifficulty = data['workoutDifficulty'] ?? workoutDifficulty;
+
+  }
+
+  // toMap()
+  Map<String, dynamic> toMap() {
+    return {
+      'workoutId': workoutId,
+      'workoutName': workoutName,
+      'workoutType': workoutType,
+      'workoutDifficulty':  workoutDifficulty,
+    };
+  }
+
+  /*Map<String, num> getPersonality() {
+    return {
+      'neuroticism': neuroticism,
+      'conscientiousness': conscientiousness,
+      'openness': openness,
+    };
+  }
+
+  List<Map<String, num>> getDynamicData() {
+    return [
+      {
+        'height': height,
+        'weight': weight,
+        'timeSpan': timeSpan,
+      },
+      Map.fromIterables(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+          List.generate(7, (index) => int.parse(workoutDays[index]))),
+      {
+        'strengthLiking': strengthLiking,
+        'cardioLiking': cardioLiking,
+        'yogaLiking': yogaLiking,
+      },
+      {
+        'strengthAbility': strengthAbility,
+        'cardioAbility': cardioAbility,
+        'yogaAbility': yogaAbility,
+      },
+    ];
+  }
+
+  @override
+  String toString() {
+    return "UserInfo{\n  email: $email\n  password: $password\n"
+        "  userName: $userName\n  gender: $gender\n"
+        "  birthday: $birthday\n  neuroticism: $neuroticism\n"
+        "  conscientiousness: $conscientiousness\n  openness: $openness\n"
+        "  height: $height\n  weight: $weight\n"
+        "  timeSpan: $timeSpan\n  workoutDays: $workoutDays\n"
+        "  strengthLiking: $strengthLiking\n  cardioLiking: $cardioLiking\n"
+        "  yogaLiking: $yogaLiking\n  strengthAbility: $strengthAbility\n"
+        "  cardioAbility: $cardioAbility\n  yogaAbility: $yogaAbility\n}";
+  }*/
+}
