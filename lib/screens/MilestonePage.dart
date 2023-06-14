8fb714fce4801bde40619a443dff225e4faa2227
Tzuying <109306024@nccu.edu.tw>
2023-06-14 16:59:35 +0800

    Modify the milestonePage

--- lib/screens/MilestonePage.dart
+++ lib/screens/MilestonePage.dart
@@ -10,8 +10,90 @@ class MilestonePage extends StatefulWidget {
 
 class _MilestonePage extends State<MilestonePage> {
   int exerciseDays = 7; //默認運動天數為7
-  double currentLevel = 1.0; // 每個人從起點出發都是0?
+  // TODO: 抓使用者設定的運動天數
+  double currentLevel = 1.0; // 每個人從起點出發都是1
   final int totalLevels = 24;
+
+  void showOptions() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('功能選擇'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // 返回至主頁的操作
+                  Navigator.of(context).pop();
+                  // TODO: 執行返回至主頁的操作
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFFFA493), // 設定按鈕的背景顏色
+                ),
+                child: Text(
+                  '返回至主頁',
+                  style: TextStyle(
+                    color: Colors.white, // 設定內部文字的顏色
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // 繼續觀看里程碑的操作
+                  Navigator.of(context).pop();
+                  // TODO: 執行繼續觀看里程碑的操作
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFFFA493), // 設定按鈕的背景顏色
+                ),
+                child: Text(''
+                    '觀看里程碑',
+                  style: TextStyle(
+                    color: Colors.white, // 設定內部文字的顏色
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('規則'),
+                        content: Text('//內容尚在思考'),
+                        actions: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Color(0xFFFFA493), // 設定按鈕的背景顏色
+                            ),
+                            child: Text('確認'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFFFA493), // 設定按鈕的背景顏色
+                ),
+                child: Text(
+                  '里程碑規則',
+                  style: TextStyle(
+                    color: Colors.white, // 設定內部文字的顏色
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   void initState() {
@@ -83,16 +165,38 @@ class _MilestonePage extends State<MilestonePage> {
                 //起點Image
                 startLevelImage: ImageParams(
                   path: "assets/images/start.jpg",
-                  size: Size(300, 100),
+                  size: Size(600, 100),
+                  imagePositionFactor: 1.0,
                 ),
-                //終點Image //這個圖一定要改掉太醜了
+                //終點Image
                 pathEndImage: ImageParams(
                   path: "assets/images/end.jpg",
-                  size: Size(100, 100),
+                  size: Size(150, 100),
+                  imagePositionFactor: 0.0,
                 ),
+                bgImagesToBePaintedRandomly: [
+                  ImageParams(
+                      path: "assets/images/star.jpg",
+                      size: Size(40, 40),
+                      repeatCountPerLevel: 0.5),
+                  ImageParams(
+                      path: "assets/images/rocket.jpg",
+                      size: Size(50, 50),
+                      repeatCountPerLevel: 0.2),
+                ],
               ),
             ),
           ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          child: Icon(
+            Icons.more_vert,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            showOptions();
+          },
         ),
       ),
     );
